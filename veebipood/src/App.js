4685cc2f8953403e03ee071eedf56af4aa8132f2
mihkelvaher <mihkel.vaher@industry62.com>
2022-10-25 16:03:32 +0300

    Uue projekti loomine

---
+++ veebipood/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="main-pilt" src="https://estonia.ee/wp-content/uploads/nobe_netist_4.jpg" alt="" />
+      <button className="nupp">Vajuta</button>
+    </div>
+  );
+}
+
+export default App;
