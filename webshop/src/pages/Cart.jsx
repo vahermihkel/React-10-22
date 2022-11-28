11d1aa364fdedc514c553049f691e9c6c2759d4a
mihkelvaher <mihkel.vaher@industry62.com>
2022-11-28 14:29:38 +0200

    Update Cart.jsx

--- webshop/src/pages/Cart.jsx
+++ webshop/src/pages/Cart.jsx
@@ -16,9 +16,25 @@ function Cart() {
 
   const calculateCartSum = () => {
     let sum = 0;
-    cart.forEach(element => sum = sum + element.price);
-    return sum;
+    cart.forEach(element => sum = sum + element.product.price * element.quantity);
+    return sum.toFixed(2);
   };
+
+  const decreaseQuantity = (index) => {
+    cart[index].quantity = cart[index].quantity - 1;
+    if (cart[index].quantity <= 0) {
+      removeFromCart(index);
+    }
+    setCart(cart.slice());
+    sessionStorage.setItem("cart", JSON.stringify(cart)); 
+  }
+
+  const increaseQuantity = (index) => {
+    // kogus = kogus + 1;
+    cart[index].quantity = cart[index].quantity + 1;
+    setCart(cart.slice()); // HTML uuendus
+    sessionStorage.setItem("cart", JSON.stringify(cart)); // sessionStorage uuendus
+  }
 
   return ( 
     <div>
@@ -29,13 +45,16 @@ function Cart() {
         { cart.length === 0 && <img className="emptyCart" src="/empty-cart.png" alt="" />}
 
         <div>
+        {/* [{"product": toode1, "quantity": 3}, {"product": toode2, "quantity": 6}] */}
           {cart.map((element, index) =>
             <div key={index}>
-              <img src={element.image} alt="" />
-              <div>{element.name}</div>
-              <div>{element.price}</div>
-              <div>{element.description}</div>
-              <div>{element.category}</div>
+              <img src={element.product.image} alt="" />
+              <div>{element.product.name}</div>
+              <div>{element.product.price.toFixed(2)} €</div>
+              <button onClick={() => decreaseQuantity(index)}>-</button>
+              <div>{element.quantity} tk</div>
+              <button onClick={() => increaseQuantity(index)}>+</button>
+              <div>{(element.product.price * element.quantity).toFixed(2)} €</div>
               <button onClick={() => removeFromCart(index)}>x</button>
             </div>
           )}
